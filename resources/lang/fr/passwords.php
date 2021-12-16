c4a5232fbfdcf818c51fd13fe7baa9485588d9b9
rspt <julien@rspt.ch>
2021-12-16 22:38:32 +0100

    Translate error messages

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous vous avons envoyé un email avec le lien de réinitialisation!',
+    'throttled' => 'Merci d\'attendre avant de réessayer.',
+    'token' => 'Le token est invalide.',
+    'user' => "Aucun compte n'existe avec cette adresse email.",
+
+];
