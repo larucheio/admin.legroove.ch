8cdf9c4179de32c2347a25e3cc2fe107b357c015
rspt <julien@rspt.ch>
2021-12-21 12:56:01 +0100

    Improve title for layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ env('APP_NAME', 'Laravel') }}</title>
 
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <script src="{{ mix('/js/app.js') }}"></script>
