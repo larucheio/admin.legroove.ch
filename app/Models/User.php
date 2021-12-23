64101a866138634b7494385fa809ef8b41eaaec0
rspt <julien@rspt.ch>
2021-12-23 18:13:51 +0100

    Remove laravel/sanctum (unused)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -21,8 +19,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'referent',
-        'referent_phone',
+        'contact',
     ];
 
     /**
@@ -33,15 +30,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 
     public function bookings()
