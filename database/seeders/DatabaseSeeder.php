0b968408453a31759702b46b14b24ddaddd1dcd1
rspt <julien@rspt.ch>
2022-02-22 00:48:14 +0100

    Add some basic seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Settings
+        DB::table('settings')->insert([
+            'booking_dateplus_min' => 30,
+            'booking_dateplus_to' => 365,
+            'activity_dateplus_from' => 1,
+            'activity_dateplus_to' => 180,
+        ]);
+
+        // Spaces
+        DB::table('spaces')->insert([
+            ['name' => 'Backstage'],
+            ['name' => 'Bar'],
+            ['name' => 'Bureau/Billetterie'],
+            ['name' => 'Foyer'],
+            ['name' => 'Loge 1'],
+            ['name' => 'Loge 2'],
+            ['name' => 'Salle'],
+            ['name' => 'Stock technique'],
+            ['name' => 'Vestiaire'],
+        ]);
     }
 }
